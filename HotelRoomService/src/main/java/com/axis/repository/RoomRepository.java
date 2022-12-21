b082af03813460db98b20709d772cf1295e6639d
deepti697 <116783856+deepti697@users.noreply.github.com>
2022-12-21 15:08:00 +0530

    Delete HotelRoomService directory

--- HotelRoomService/src/main/java/com/axis/repository/RoomRepository.java
+++
@@ -1,9 +0,0 @@
-package com.axis.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.axis.model.Room;
-
-public interface RoomRepository extends JpaRepository<Room, Integer>{
-
-}
